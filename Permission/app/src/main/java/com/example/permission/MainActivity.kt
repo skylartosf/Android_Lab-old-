fe9a2375aea2f2cdde6c1f83e580e239e128e16a
Skylar <olfaminion0116@naver.com>
2023-03-28 07:02:56 +0900

    [127] Ask for only 1 permission(camera)

--- Permission/app/src/main/java/com/example/permission/MainActivity.kt
+++ Permission/app/src/main/java/com/example/permission/MainActivity.kt
@@ -1,13 +1,41 @@
 package com.example.permission
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val cameraResultLauncher: ActivityResultLauncher<String> =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                Toast.makeText(this, "Permission granted for camera", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Permission denied for camera", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val btnCamPermission: Button = findViewById(R.id.btn_camera_permission)
+        btnCamPermission.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
+                shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)
+            ) {
+                //TODO: showRationaleDialog() not working
+            } else { // ask for permission
+                cameraResultLauncher.launch(Manifest.permission.CAMERA)
+            }
+        }
 
     }
 }
