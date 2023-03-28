e699e2d23e15a9350a70339af5c4752ee555c4c6
Skylar <twinkle991@naver.com>
2023-03-28 13:28:11 +0900

    [134] coroutine structure, with progress dialog

--- Coroutine_1/app/src/main/java/com/example/coroutine_1/MainActivity.kt
+++ Coroutine_1/app/src/main/java/com/example/coroutine_1/MainActivity.kt
@@ -1,22 +1,32 @@
 package com.example.coroutine_1
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.Toast
-import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import androidx.lifecycle.lifecycleScope
+
+/* coroutine 사용법 !
+- suspend func + withContext(그 func이 어디 thread에서 돌지)
+- 어느 scope(시기)에서 실행? ViewModelScope/LifeCycleScope(/Livedata).launch { 그 func 호출 }
+ */
 
 class MainActivity : AppCompatActivity() {
+
+    var customProgressDialog: Dialog? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val btnExec: Button = findViewById(R.id.btn_execute)
         btnExec.setOnClickListener {
+            showProgressDialog()
             // You can't exec a suspend func on the main thread
             // exec("Task executed successfully!")
             // lifecycleScope : coroutine block built to properly handle coroutines in any Activity class
@@ -39,15 +49,33 @@ class MainActivity : AppCompatActivity() {
             for (i in 1..100000) {
                 Log.e("delay : ", "" + i)
             }
-            Toast.makeText(this@MainActivity, result, Toast.LENGTH_SHORT).show()
+
+            runOnUiThread {
+                cancelProgressDialog()
+                Toast.makeText(this@MainActivity, result, Toast.LENGTH_LONG).show()
+            }
+            Log.e("delay : ", "finally finished!")
             // why app crashes?
             // we're trying to run this Toast on our background thread
             // but Toast is a foreground or a UI task
             // so such an UI-related action can only be run on the UI thread
             // but we're running it on Dispatchers.IO thread
+            // -> if we make it runOnUiThread, app will not crash
         }
 
+    }
 
+    private fun cancelProgressDialog() {
+        if (customProgressDialog != null) {
+            customProgressDialog?.dismiss()
+            customProgressDialog = null
+        }
+    }
+
+    private fun showProgressDialog() {
+        customProgressDialog = Dialog(this@MainActivity)
+        customProgressDialog?.setContentView(R.layout.dialog_custom_progress)
+        customProgressDialog?.show()
     }
 
 }
